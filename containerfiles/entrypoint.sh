18cd09c39c2c1b0ff249ff3c1e6f12e65cea6d16
Jasper Snel <me@jaspersnel.me>
2021-06-02 13:22:06 +0200

    Fix entities import

--- containerfiles/entrypoint.sh
+++ containerfiles/entrypoint.sh
@@ -1,2 +1,5 @@
+# wget -O /vantage6/vantage6-server/vantage6/server/controller/fixture.py https://raw.githubusercontent.com/jaspersnel/vantage6-server/collab-encryption-fix/vantage6/server/controller/fixture.py
+vserver-local start -c /config.yaml &
+sleep 5
 vserver-local import -c /config.yaml /entities.yaml
-vserver-local start -c /config.yaml
+tail -f /dev/null
